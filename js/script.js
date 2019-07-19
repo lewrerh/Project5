3fe2afea7d241c07e358f7f10f5506afc5eecc8a
Rhonda Lawrence <rhonda.lawrence@verizon.com>
2019-07-19 08:58:42 -0500

    Working steps for card images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-$(document).ready(function () {
+//$(document).ready(function () {//
+let employeeCards = [];
+//let gallery = $("#gallery");//
 
-    let gallery = $("#gallery");
-
-    $.ajax({
-        url: 'https://randomuser.me/api/?results=12',
-        dataType: 'json',
-        success: function (data) {
-
-            console.log(data);
-        }
-    });
+$.ajax({
+    url: 'https://randomuser.me/api/?results=12',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        employeeCards = data.results;
+        createEmployeeCard(employeeCards);
+    }
 });
+//Create function with for loop//
+function createEmployeeCard(employeeCards) {
 
+
+    for (let i = 0; i < employeeCards.length; i++) {
+        let card =
+            `<div class="card" index="${i}">
+                <div class="card-img-container">
+                <img class="modal-img" src="${employeeCards[i].picture.medium}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name0" class="card-name cap">${employeeCards[i].name.first} ${employeeCards[i].name.last}</h3>
+                    <p class="card-text">email${employeeCards[i].email}</p>
+                    <p class="card-text cap">${employeeCards[i].location.city}, ${employeeCards[i].location.city}</p>
+                </div>
+            </div>`
+        $(".gallery").append(card);
+
+
+    };
+}
